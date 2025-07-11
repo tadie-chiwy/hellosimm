fa2719f932dc949c55c23f7829a19a9bff354b66
tadie-chiwy <127861828+tadie-chiwy@users.noreply.github.com>
2025-07-11 11:20:48 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let menu = document.querySelector('.menu');
+let hd = document.querySelector('header');
+
+menu.addEventListener('click',function(){
+    hd.classList.toggle('open');
+})
+
+console.log('tdd');
